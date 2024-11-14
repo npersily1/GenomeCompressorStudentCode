dd2199bd552d450cb5ba7ea1feab84d1177112c3
npersily1 <noah.persily@menloschool.org>
2024-11-13 19:21:15 -0800

    Worklog

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,9 @@
-Name: YOUR NAME HERE
+Name: Noah Persily
 
-| Date    |  Time  |                                     Update |
-|:--------|:------:|-------------------------------------------:|
-| April 5 | 9-10pm | I tried to write abstract, efficient code. |
-|         |        |                                            |
+| Date        |     Time     |                                                                                                                                                                                                                    Update |
+|:------------|:------------:|--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------:|
+| November 13 | 0645-0715 pm | I created 2 maps each that associate as number (0,1,2,3) to a letter (A,C,G,T) and another map which inverts the key value pairs. Then I say while not empty write the associated map value to the current read in value. |
+|             |              |                                                                                                                                                                                                                           |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/GenomeCompressor.java
+++ src/GenomeCompressor.java
@@ -10,12 +10,12 @@
  ******************************************************************************/
 
 /**
- *  The {@code GenomeCompressor} class provides static methods for compressing
- *  and expanding a genomic sequence using a 2-bit code.
+ * The {@code GenomeCompressor} class provides static methods for compressing
+ * and expanding a genomic sequence using a 2-bit code.
  *
- *  @author Robert Sedgewick
- *  @author Kevin Wayne
- *  @author Zach Blick
+ * @author Robert Sedgewick
+ * @author Kevin Wayne
+ * @author Zach Blick
  */
 public class GenomeCompressor {
 
@@ -23,9 +23,19 @@ public class GenomeCompressor {
      * Reads a sequence of 8-bit extended ASCII characters over the alphabet
      * { A, C, T, G } from standard input; compresses and writes the results to standard output.
      */
-    public static void compress() {
 
-        // TODO: complete the compress() method
+    public static void compress() {
+        int[] map = new int[128];
+        map['A'] = 0b00;
+        map['C'] = 0b01;
+        map['G'] = 0b10;
+        map['T'] = 0b11;
+
+
+        while (!BinaryStdIn.isEmpty()) {
+            char c = BinaryStdIn.readChar();
+            BinaryStdOut.write(map[c], 2);
+        }
 
         BinaryStdOut.close();
     }
@@ -36,9 +46,22 @@ public class GenomeCompressor {
     public static void expand() {
 
         // TODO: complete the expand() method
+        char[] map = new char[4];
+        map[0b00] = 'A';
+        map[0b01] = 'C';
+        map[0b10] = 'T';
+        map[0b11] = 'G';
+
+        BinaryStdOut.close();
+        while (!BinaryStdIn.isEmpty()) {
+            int i = BinaryStdIn.readInt(2);
+            BinaryStdOut.write(map[i]);
+        }
 
         BinaryStdOut.close();
     }
+
+
 
 
     /**
@@ -49,7 +72,7 @@ public class GenomeCompressor {
      */
     public static void main(String[] args) {
 
-        if      (args[0].equals("-")) compress();
+        if (args[0].equals("-")) compress();
         else if (args[0].equals("+")) expand();
         else throw new IllegalArgumentException("Illegal command line argument");
     }
